3ad8bb5a481d9420bbc7c62e3be5b34fa9e2636b
magosil86 <magosil86@gmail.com>
2015-11-24 15:04:04 +0000

    Create QuickstartUserInput.py

---
+++ QuickstartUserInput.py
@@ -0,0 +1,18 @@
+#!/bin/env python
+
+""" QuickstartUserInput.py 
+
+    -Configuration file for the user to supply the projectname, author,  
+    and cutoffs specific to pipeline_quickstart.py
+=============================================================================
+"""
+
+# settings for pipeline_quickstart.py
+#==========================================
+
+projectname = ''
+
+author = ''
+
+
+
